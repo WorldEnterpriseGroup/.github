0080c37cc635ddecb930ee9d7a0469e43660c39a
Björn Mage <matthew@taoninja.com>
2023-03-11 16:29:25 -0500

    Structure

---
+++ .github/workflows/openai/function.py
@@ -0,0 +1,16 @@
+import os
+import openai
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "Hello!"}
+    {"role": "system", "content": "Hello!"}
+    {"role": "user", "content": "Hello!"}
+  ]
+)
+
+print(completion.choices[0].message)
+
+
